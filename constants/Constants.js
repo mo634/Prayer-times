e35b9a554308ab5d9eee6e483feb0a89bb887788
MohamedMostafa <mido221gyefue@gmail.com>
2023-10-17 12:08:30 +0300

    Updates

--- constants/Constants.js
+++ constants/Constants.js
@@ -8,12 +8,4 @@ export const prayerNames = {
     "isha": "العشاء",
 }
 
-export const prayerImage = {
-    fajrScr:"/images/fajr-prayer.png",
-    dhuhrScr:"/images/dhhr-prayer-mosque.png",
-    asrScr:"/images/asr-prayer-mosque.png",
-    maghribScr:"/images/sunset-prayer-mosque.png",
-    ishaScr:"/images/night-prayer-mosque.png",
-}
-
 export const cityArr=["القاهره","الفيوم","الاسكندرية","الاسماعلية"]
